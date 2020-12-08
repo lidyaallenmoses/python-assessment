60875bcb879c2a6277e57984ed60f2be0188bf90
lidyaallenmoses <72965147+lidyaallenmoses@users.noreply.github.com>
2020-12-08 22:29:09 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,7 +1,18 @@
-start =int(input("enter start number: \t"))
-end =int(input("enter end number: \t"))
-for n in range(start,end+1):
-	if n % 2 ==0:
-		print("even number",n )
-	else:
-		print("odd number",n)
+n=int(input("Enter a Number: "))
+value= n%2==0
+if(value):
+    print("Even: ", end=" ")
+    for i in range(n,n+10,2):
+        print(i, end=" ")
+    print()
+    print("Odd: ", end=" ")
+    for i in range(n+1,n+10,2):
+        print(i, end=" ")
+else:
+    print("Odd: ", end=" ")
+    for i in range(n,n+10,2):
+        print(i, end=" ")
+    print()
+    print("Even: ", end=" ")
+    for i in range(n+1,n+10,2):
+        print(i, end=" ")    
