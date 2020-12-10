a970789a242376a457a29f55acfb686798d0a381
lidyaallenmoses <72965147+lidyaallenmoses@users.noreply.github.com>
2020-12-10 08:21:42 +0530

    Create recursionFunction.py

---
+++ Day2/recursionFunction.py
@@ -0,0 +1,9 @@
+def recur(num):
+    if num<=10:
+        print(f'{num} ', end=' ')
+        recur(num+1)
+        #print(f'{num} ', end=' ')
+        
+n = int(input('Enter a number: '))
+print("The Recursion of", n, "is")
+recur(n)
